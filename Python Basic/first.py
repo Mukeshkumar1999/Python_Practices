4f7d43cd99cb1c61cbd88fc41b49a823a4b8dde2
Mukesh kumar paswan <mukeshk92578@gamil.com>
2025-09-23 01:01:36 +0530

    23/09/2025

---
+++ Python Basic/first.py
@@ -0,0 +1,46 @@
+print("Radhe Radhe")
+
+# a = 10
+# b = 20
+# print(a+b)
+# print("val of a", a)
+
+# def showName(str):
+#     print(str)
+# showName("Radhe Radhe")
+
+# write program to add two number
+'''
+def addNum(a,b):
+    return a+b
+
+print(addNum(10,20))
+'''   #multiline comments
+
+# for loop
+'''
+for i in range(0,10):
+    print("val of i", i)
+'''
+# print number 1 to 10 using function
+'''
+def tablePrint(a, st,end):
+    for i in range(st,end):
+        print('Table of ',a,'*',i,':', a * i )
+tablePrint(2,1,11)
+
+'''
+# print fibbonacci series
+# 0,1,1,2,3,5,8,13....
+
+a = 0
+b = 1
+
+for i in range(0,4):
+    print(a)
+    c = a+b;
+    a = b
+    b = c
+
+
+
